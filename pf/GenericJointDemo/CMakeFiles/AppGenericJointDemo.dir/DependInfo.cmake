f7e17493e4d661ff6e74e21ff602728776454320
Tomas <tomymehdi@gmail.com>
2015-04-30 21:24:44 -0300

    pbproyect valid

---
+++ pf/GenericJointDemo/CMakeFiles/AppGenericJointDemo.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Tom/itba/pf/bullet3-master/Demos/GenericJointDemo/GenericJointDemo.cpp" "/Users/Tom/itba/pf/bullet3-master/Demos/GenericJointDemo/CMakeFiles/AppGenericJointDemo.dir/GenericJointDemo.o"
+  "/Users/Tom/itba/pf/bullet3-master/Demos/GenericJointDemo/Ragdoll.cpp" "/Users/Tom/itba/pf/bullet3-master/Demos/GenericJointDemo/CMakeFiles/AppGenericJointDemo.dir/Ragdoll.o"
+  "/Users/Tom/itba/pf/bullet3-master/Demos/GenericJointDemo/main.cpp" "/Users/Tom/itba/pf/bullet3-master/Demos/GenericJointDemo/CMakeFiles/AppGenericJointDemo.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "USE_GRAPHICAL_BENCHMARK"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/Tom/itba/pf/bullet3-master/Demos/OpenGL/CMakeFiles/OpenGLSupport.dir/DependInfo.cmake"
+  "/Users/Tom/itba/pf/bullet3-master/src/BulletDynamics/CMakeFiles/BulletDynamics.dir/DependInfo.cmake"
+  "/Users/Tom/itba/pf/bullet3-master/src/BulletCollision/CMakeFiles/BulletCollision.dir/DependInfo.cmake"
+  "/Users/Tom/itba/pf/bullet3-master/src/LinearMath/CMakeFiles/LinearMath.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "Demos/GenericJointDemo/../OpenGL"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/GLUT.framework/Headers"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ pf/GenericJointDemo/CMakeFiles/AppGenericJointDemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AppGenericJointDemo.dir/GenericJointDemo.o"
+  "CMakeFiles/AppGenericJointDemo.dir/Ragdoll.o"
+  "CMakeFiles/AppGenericJointDemo.dir/main.o"
+  "AppGenericJointDemo.pdb"
+  "AppGenericJointDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AppGenericJointDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
