a60314908089b20ac67c9c7d6a522dd6012a42b5
Enzo Altamiranda <enzo.alt@gmail.com>
2015-08-02 18:16:25 -0300

    Avanzando sobre la parametrización.

--- src/BodyParameters.h
+++ src/BodyParameters.h
@@ -12,39 +12,41 @@
 #include <stdio.h>
 #include <BulletDynamics/btBulletDynamicsCommon.h>
 
+#define     BODY_SCALE      1
 
 class BodyParameters {
-
+    
 public:
-    double bodyMultiplier = 1;
+    
+    /******************** PARTS ********************/
     
     btVector3 positionOffset = btVector3(0, -10, 0);
     
     // Leg body group
     // lower leg
-    double L_LEG_RADIUS = 0.05;
-    double L_LEG_HEIGHT = 0.37;
-    double L_LEG_MASS = 1;
+    double L_LEG_RADIUS = 0.05 * BODY_SCALE;
+    double L_LEG_HEIGHT = 0.37 * BODY_SCALE;
+    double L_LEG_MASS = 1 * BODY_SCALE;
     btVector3 L_LEG_ORIENTATION = btVector3(0, 0, 0);
-    btVector3 L_LEG_POSITION = btVector3(btScalar(0.18),
-                                         btScalar(0.2),
+    btVector3 L_LEG_POSITION = btVector3(btScalar(0.18 * BODY_SCALE),
+                                         btScalar(0.2 * BODY_SCALE),
                                          btScalar(0.0));
     
     //upper leg
-    double U_LEG_RADIUS = 0.07;
-    double U_LEG_HEIGHT = 0.45;
-    double U_LEG_MASS = 1;
+    double U_LEG_RADIUS = 0.07 * BODY_SCALE;
+    double U_LEG_HEIGHT = 0.45 * BODY_SCALE;
+    double U_LEG_MASS = 1 * BODY_SCALE;
     btVector3 U_LEG_ORIENTATION = btVector3(0, 0, 0);
-    btVector3 U_LEG_POSITION = btVector3(btScalar(0.18),
-                                         btScalar(0.65),
+    btVector3 U_LEG_POSITION = btVector3(btScalar(0.18 * BODY_SCALE),
+                                         btScalar(0.65 * BODY_SCALE),
                                          btScalar(0.));
     
-    double FOOT_RADIUS = 0.05;
-    double FOOT_HEIGHT = 0.15;
-    double FOOT_MASS = 1;
+    double FOOT_RADIUS = 0.05 * BODY_SCALE;
+    double FOOT_HEIGHT = 0.15 * BODY_SCALE;
+    double FOOT_MASS = 1 * BODY_SCALE;
     btVector3 FOOT_ORIENTATION = btVector3(SIMD_HALF_PI, 0, 0);
-    btVector3 FOOT_POSITION = btVector3(btScalar(0.18),
-                                        btScalar(0.2),
+    btVector3 FOOT_POSITION = btVector3(btScalar(0.18 * BODY_SCALE),
+                                        btScalar(0.2 * BODY_SCALE),
                                         btScalar(0.));
     
     // Arm body group
@@ -58,43 +60,63 @@ public:
                                          btScalar(0));
     
     //upper arm
-    double U_ARM_RADIUS = 0.05;
-    double U_ARM_HEIGHT = 0.33;
-    double U_ARM_MASS = 1;
+    double U_ARM_RADIUS = 0.05 * BODY_SCALE;
+    double U_ARM_HEIGHT = 0.33 * BODY_SCALE;
+    double U_ARM_MASS = 1 * BODY_SCALE;
     btVector3 U_ARM_ORIENTATION = btVector3(0, 0, -SIMD_HALF_PI);
-    btVector3 U_ARM_POSITION = btVector3(btScalar(0.35),
-                                         btScalar(1.45),
+    btVector3 U_ARM_POSITION = btVector3(btScalar(0.35 * BODY_SCALE),
+                                         btScalar(1.45 * BODY_SCALE),
                                          btScalar(0));
     
     // Torso body group
     // Pelvis
-    double PELVIS_RADIUS = 0.15;
-    double PELVIS_HEIGHT = 0.20;
-    double PELVIS_MASS = 1.0;
+    double PELVIS_RADIUS = 0.15 * BODY_SCALE;
+    double PELVIS_HEIGHT = 0.20 * BODY_SCALE;
+    double PELVIS_MASS = 1.0 * BODY_SCALE;
     btVector3 PELVIS_ORIENTATION = btVector3(0, 0, 0);
     btVector3 PELVIS_POSITION = btVector3(btScalar(0),
-                                          btScalar(1),
+                                          btScalar(1 * BODY_SCALE),
                                           btScalar(0));
     
     // Spine
-    double SPINE_RADIUS = 0.15;
-    double SPINE_HEIGHT = 0.28;
-    double SPINE_MASS = 1.0;
+    double SPINE_RADIUS = 0.15 * BODY_SCALE;
+    double SPINE_HEIGHT = 0.28 * BODY_SCALE;
+    double SPINE_MASS = 1.0 * BODY_SCALE;
     btVector3 SPINE_ORIENTATION = btVector3(0, 0, 0);
     btVector3 SPINE_POSITION = btVector3(btScalar(0),
-                                         btScalar(1.6),
+                                         btScalar(1.6 * BODY_SCALE),
                                          btScalar(0));
     
     // HEAD
-    double HEAD_RADIUS = 0.10;
-    double HEAD_HEIGHT = 0.05;
-    double HEAD_MASS = 1.0;
+    double HEAD_RADIUS = 0.10 * BODY_SCALE;
+    double HEAD_HEIGHT = 0.05 * BODY_SCALE;
+    double HEAD_MASS = 1.0 * BODY_SCALE;
     btVector3 HEAD_ORIENTATION = btVector3(0, 0, 0);
     btVector3 HEAD_POSITION = btVector3(btScalar(0),
-                                        btScalar(1.6),
+                                        btScalar(1.6 * BODY_SCALE),
                                         btScalar(0));
     
+    /******************** CONTRAINTS ********************/
     
+    // LEFT ANKLE
+    
+    // RIGHT ANKLE
+    
+    // LEFT KNEE
+    // RIGHT KNEE
+    
+    // LEFT HIP
+    // RIGHT HIP
+    
+    // SPINE-PELVIS
+    
+    // NECK
+    
+    // LEFT SHOULDER
+    // RIGHT SHOULDER
+    
+    // LEFT ARM
+    // RIGHT ARM
     
 };
 
