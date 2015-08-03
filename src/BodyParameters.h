bc0860cebab95134ddcc10adef74ca96a9cadee6
Enzo Altamiranda <enzo.alt@gmail.com>
2015-08-03 12:15:54 -0300

    Casi terminada la parametrización.

--- src/BodyParameters.h
+++ src/BodyParameters.h
@@ -11,12 +11,21 @@
 
 #include <stdio.h>
 #include <BulletDynamics/btBulletDynamicsCommon.h>
+#include "Actuator.h"
 
 #define     BODY_SCALE      1
 
 class BodyParameters {
     
 public:
+    
+    /******************** ACTUATORS ********************/
+
+    Actuator rightLowerLegAct, rightUpperLegAct;
+    Actuator leftLowerLegAct, leftUpperLegAct;
+    Actuator hipAct;
+    Actuator rightLowerArmAct, rightUpperArmAct;
+    Actuator leftLowerArmAct, leftUpperArmAct;
     
     /******************** PARTS ********************/
     
@@ -98,26 +107,115 @@ public:
     
     /******************** CONTRAINTS ********************/
     
-    // LEFT ANKLE
     
-    // RIGHT ANKLE
+    // ANKLE
+    btVector3 ankleFootOffset = btVector3(btScalar(0),
+                                          btScalar(0.185 * BODY_SCALE),
+                                          btScalar(0));
+    btVector3 ankleLowerLegOffset = btVector3(btScalar(0),
+                                              btScalar(-0.225 * BODY_SCALE),
+                                              btScalar(0));
+    btVector3 ankleAngularLowerLimit = btVector3(SIMD_EPSILON * 0.5, SIMD_EPSILON, SIMD_EPSILON);
+    btVector3 ankleAngularUpperLimit = btVector3(-SIMD_PI, -SIMD_EPSILON, -SIMD_EPSILON);
     
-    // LEFT KNEE
-    // RIGHT KNEE
     
-    // LEFT HIP
-    // RIGHT HIP
+    // KNEE
+    btVector3 kneeUpperLegOffset = btVector3(btScalar(0),
+                                             btScalar(-0.225 * BODY_SCALE),
+                                             btScalar(0));
+    btVector3 kneeLowerLegOffset = btVector3(btScalar(0),
+                                             btScalar(0.185),
+                                             btScalar(0));
+    btVector3 kneeAngularLowerLimit = btVector3(-SIMD_EPSILON, -SIMD_EPSILON, -SIMD_EPSILON);
+    btVector3 kneeAngularUpperLimit = btVector3(SIMD_PI * 0.7, SIMD_EPSILON, SIMD_EPSILON);
+    
     
     // SPINE-PELVIS
+    btVector3 pelvisOffset = btVector3(btScalar(0.),
+                                       btScalar(0.15 * BODY_SCALE),
+                                       btScalar(0.));
+    btVector3 lowerSpineOffset = btVector3(btScalar(0.),
+                                           btScalar(-0.15 * BODY_SCALE),
+                                           btScalar(0.));
+    
+    btVector3 hipLowerAngularLimit = btVector3(-SIMD_PI * 0.2, -SIMD_EPSILON, -SIMD_PI * 0.3);
+    btVector3 hipUpperAngularLimit = btVector3(SIMD_PI * 0.2, SIMD_EPSILON, SIMD_PI * 0.6);
+    
+    btVector3 hipPelvis_eulerZYX = btVector3(0, SIMD_HALF_PI, 0);
+    btVector3 hipSpine_eulerZYX = btVector3(0, SIMD_HALF_PI, 0);
+    
     
     // NECK
+    btVector3 neckUpperSpineOffset = btVector3(btScalar(0.),
+                                               btScalar(0.30 * BODY_SCALE),
+                                               btScalar(0.));
+    btVector3 headOffset = btVector3(btScalar(0.),
+                                     btScalar(-0.14 * BODY_SCALE),
+                                     btScalar(0.));
+    
+    btVector3 neckLowerAngularLimit = btVector3(-SIMD_PI * 0.3f, -SIMD_EPSILON, -SIMD_PI * 0.3f);
+    btVector3 neckUpperAngularLimit = btVector3(SIMD_PI * 0.5f, SIMD_EPSILON, SIMD_PI * 0.3f);
+    
+    
+    // ELBOW
+    btVector3 elbowUpperOffset = btVector3(btScalar(0), btScalar(0.18 * BODY_SCALE), 0);
+    btVector3 elbowLowerOffset = btVector3(btScalar(0), btScalar(-0.14 * BODY_SCALE), 0);
+    btVector3 elbowAngularUpperLimit = btVector3(-SIMD_EPSILON, -SIMD_EPSILON, -SIMD_EPSILON);
+    btVector3 elbowAngularLowerLimit = btVector3(SIMD_PI * 0.7, SIMD_EPSILON, SIMD_EPSILON);
     
     // LEFT SHOULDER
+    btVector3 leftShoulderTorsoOffset = btVector3(btScalar(-0.2 * BODY_SCALE),
+                                                  btScalar(0.15 * BODY_SCALE),
+                                                  btScalar(0.));
+    
+    btVector3 leftShoulderLeftArmOffset = btVector3(btScalar(0.),
+                                                    btScalar(-0.18 * BODY_SCALE),
+                                                    btScalar(0.));
+    
+    btVector3 leftShoulderLeftArmEulerZYX = btVector3(SIMD_HALF_PI, 0, -SIMD_HALF_PI);
+    
+    btVector3 leftShoulderAngularLowerLimit = btVector3(-SIMD_PI * 0.8, -SIMD_EPSILON, -SIMD_PI * 0.5);
+    btVector3 leftShoulderAngularUpperLimit = btVector3(SIMD_PI * 0.8, SIMD_EPSILON, SIMD_PI * 0.5);
+    
     // RIGHT SHOULDER
+    btVector3 rightShoulderTorsoOffset = btVector3(btScalar(0.2 * BODY_SCALE),
+                                                   btScalar(0.15 * BODY_SCALE),
+                                                   btScalar(0.));
     
-    // LEFT ARM
-    // RIGHT ARM
+    btVector3 rightShoulderRightArmOffset = btVector3(btScalar(0.),
+                                                      btScalar(-0.18 * BODY_SCALE),
+                                                      btScalar(0.));
     
+    btVector3 rightShoulderRightArmEulerZYX = btVector3(0, 0, SIMD_HALF_PI);
+    
+    btVector3 rightShoulderAngularLowerLimit = btVector3(-SIMD_PI * 0.8, -SIMD_EPSILON, -SIMD_PI * 0.5);
+    btVector3 rightShoulderAngularUpperLimit = btVector3(SIMD_PI * 0.8, SIMD_EPSILON, SIMD_PI * 0.5);
+    
+    
+    // LEFT HIP
+    btVector3 leftHipTorsoOffset = btVector3(btScalar(-0.18 * BODY_SCALE),
+                                             btScalar(-0.10 * BODY_SCALE),
+                                             btScalar(0.));
+    
+    btVector3 leftHipOffset = btVector3(btScalar(0.),
+                                        btScalar(0.225 * BODY_SCALE),
+                                        btScalar(0.));
+    
+    btVector3 leftHipAngularUpperLimit = btVector3(SIMD_HALF_PI * 0.8, SIMD_EPSILON, SIMD_HALF_PI * 0.6);
+    btVector3 leftHipAngularLowerLimit = btVector3(-SIMD_HALF_PI * 0.5, -SIMD_EPSILON, -SIMD_HALF_PI);
+    
+    
+    // RIGHT HIP
+    btVector3 rightHipTorsoOffset = btVector3(btScalar(0.18 * BODY_SCALE),
+                                              btScalar(-0.10 * BODY_SCALE),
+                                              btScalar(0.));
+    
+    btVector3 rightHipOffset = btVector3(btScalar(0.),
+                                         btScalar(0.225 * BODY_SCALE),
+                                         btScalar(0.));
+    
+    btVector3 rightHipAngularUpperLimit = btVector3(SIMD_HALF_PI * 0.8, SIMD_EPSILON, SIMD_HALF_PI);
+    btVector3 rightHipAngularLowerLimit = btVector3(-SIMD_HALF_PI * 0.5, -SIMD_EPSILON, -SIMD_HALF_PI * 0.6);
 };
 
 #endif /* defined(__test__BodyParameters__) */
