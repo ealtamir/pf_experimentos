d471ca72bcf730a9a2e73d35804b7a5e6b712a71
Tomas Mehdi <tomymehdi@Tomass-MacBook-Pro.local>
2015-10-06 19:39:57 -0300

    Matlab plotter

--- plot_output.m
+++ plot_output.m
@@ -1,2 +1,17 @@
+% Put this inside bin/Debug and execute it to plot the mean per generation
+
 data = csvread('output.dat');
-plot(data(:,1), data(:,2))
+
+generations = 1000;
+populationSize = 20;
+fitneses = data(:,2);
+boxplotMatrix = zeros(populationSize, generations);
+
+for i = 1:generations
+    for j = 1:populationSize
+        boxplotMatrix(j,i) = fitneses((i-1)*populationSize+j);
+    end
+end
+
+% boxplot(boxplotMatrix)
+plot(1:generations, mean(boxplotMatrix))
